3f34458261538ebd90a03826812a2f455911b5b3
Clarissa Estremos <clarissa@ideyatech.com>
2020-02-13 14:27:37 +0800

    #2649: Defined each value property by using Short-circuit evaluation, to prevent error on edit

--- src/modules/users/forms/EditUserForm.jsx
+++ src/modules/users/forms/EditUserForm.jsx
@@ -26,31 +26,31 @@ const EditUserForm = props => {
                     <Form.Group>
                         <Form.Label>Name</Form.Label>
                         <Form.Control as="input"
-                            type="text" name="name" value={user?.name} onChange={handleInputChange}>
+                            type="text" name="name" value={user?.name || '' } onChange={handleInputChange}>
                         </Form.Control>
                     </Form.Group>
                     <Form.Group>
                         <Form.Label>Username</Form.Label>
                         <Form.Control as="input"
-                            type="text" name="username" value={user?.username} onChange={handleInputChange}>
+                            type="text" name="username" value={user?.username || ''} onChange={handleInputChange}>
                         </Form.Control>
                     </Form.Group>
                     <Form.Group>
                         <Form.Label>Email</Form.Label>
                         <Form.Control as="input"
-                            type="email" name="email" value={user?.email} onChange={handleInputChange}>
+                            type="email" name="email" value={user?.email || ''} onChange={handleInputChange}>
                         </Form.Control>
                     </Form.Group>
                     <Form.Group>
                         <Form.Label>Contact No.</Form.Label>
                         <Form.Control as="input"
-                            type="text" name="contactNo" value={user?.contactNo} onChange={handleInputChange}>
+                            type="text" name="contactNo" value={user?.contactNo || ''} onChange={handleInputChange}>
                         </Form.Control>
                     </Form.Group>
                     <Form.Group>
                         <Form.Label>Venue</Form.Label>
                         <Form.Control as="select"
-                            className='custom-select' name="status" value={user?.status} onChange={handleInputChange}>
+                            className='custom-select' name="status" value={user?.status || ''} onChange={handleInputChange}>
                             <option value='default' disabled> Choose a status ... </option>
                             <option value="Active"> Active </option>
                             <option value="Inactive"> Inactive</option>
