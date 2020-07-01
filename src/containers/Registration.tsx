94eda434a8e9730d66f116ef3ea969c4108492f7
mlph-pyap <pyap@ideyatech.com>
2020-07-01 17:19:16 +0800

    trial test on bug fixing in onchange event

--- src/containers/Registration.tsx
+++ src/containers/Registration.tsx
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import { Button, FormControl, Form } from "react-bootstrap";
 import styled from 'styled-components';
 
+let test={
+  "email":false,
+  "password":false
+}
 export default function Registration () {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -13,27 +17,41 @@ export default function Registration () {
     function handleSubmit( event:React.FormEvent<HTMLFormElement> ) {
       event.preventDefault();
     }
-    
-    const Registration = styled.div`padding: 60px 0;`;
+    function onKeyDown(event){
+        console.log("Asd",event.currentTarget.id);
+        test[event.currentTarget.id]=true;
+    }
+    function onKeyUp(event){
+      console.log("Asd",event.currentTarget.id);
+      test[event.currentTarget.id]=false;
+    }
+    const Registrationdiv = styled.div`padding: 60px 0;`;
     const RegistrationH3 = styled.h3`text-align: center;`;
     const RegistrationForm = styled.h3`margin: 0 auto;
     max-width: 320px;`;
     return (
         
-      <Registration>
+      <Registrationdiv>
         <RegistrationH3>Registration</RegistrationH3>
         <RegistrationForm onSubmit={handleSubmit}>
           <Form.Group controlId="email">
             <FormControl
-              autoFocus
+              autoFocus={test['email']}
+              tabIndex={0}
               type="email"
               value={email}
+              onKeyDown ={onKeyDown}
+              onKeyUp ={onKeyUp}
               onChange={e => setEmail(e.target.value)}
             />
           </Form.Group>
           <Form.Group controlId="password">
             <FormControl
+            autoFocus={test['password']}
+              tabIndex={1}
               value={password}
+              onKeyDown ={onKeyDown}
+              onKeyUp ={onKeyUp}
               onChange={e => setPassword(e.target.value)}
               type="password"
             />
@@ -42,6 +60,6 @@ export default function Registration () {
             Register
           </Button>
         </RegistrationForm>
-      </Registration>
+      </Registrationdiv>
     );
   }
