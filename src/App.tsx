a758c17e0fdacf3a3e02a1a8c2ddb27157cc28ab
mlph-pyap <pyap@ideyatech.com>
2020-07-03 11:59:08 +0800

    Adding redirect function for not matching routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Redirect,useHistory,useLocation } from 'react-router-dom'
 import RoutePaths from 'constants/RoutePaths'
 import { MainScreen, Login, Registration } from 'containers'
 
@@ -12,10 +12,31 @@ export default function App() {
           <Route path={ RoutePaths.LOGIN } component={ Login }></Route>
           <Route path={ RoutePaths.REGISTER } component={ Registration }></Route>
           <Route path={ RoutePaths.MAIN } component={ MainScreen }></Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+
+          
           <Redirect exact from={ RoutePaths.BASE }  to={ RoutePaths.LOGIN } />
         </Switch>
       </Router>
       
     </div>
   )
+}
+
+function NoMatch() {
+  let history = useHistory();
+  let location = useLocation();
+   
+  history.replace(RoutePaths.LOGIN);
+  
+  
+  return (
+    <div>
+      <h3>
+        No match for <code>{location.pathname}</code>
+      </h3>
+    </div>
+  );
 }
