fb0bad1c16639c4daeb9bafd97c8b9264afa97d3
mlph-pyap <pyap@ideyatech.com>
2020-07-20 14:27:07 +0800

    Starting to apply theme paperbase on event registration

---
+++ src/components/themes/Content.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Tooltip from '@material-ui/core/Tooltip';
+import IconButton from '@material-ui/core/IconButton';
+import { createStyles, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
+import RefreshIcon from '@material-ui/icons/Refresh';
+
+
+import {Route, Switch, Redirect} from 'react-router-dom';
+import RoutePaths from 'constants/RoutePaths';
+import { DashboardContainer, UserContainer, VenueContainer, ReservationContainer } from 'containers';
+
+
+const styles = (theme: Theme) =>
+  createStyles({
+    paper: {
+      maxWidth: 936,
+      margin: 'auto',
+      overflow: 'hidden',
+    },
+    searchBar: {
+      borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
+    },
+    searchInput: {
+      fontSize: theme.typography.fontSize,
+    },
+    block: {
+      display: 'block',
+    },
+    addUser: {
+      marginRight: theme.spacing(1),
+    },
+    contentWrapper: {
+      margin: '40px 16px',
+    },
+  });
+
+export interface ContentProps extends WithStyles<typeof styles> {}
+
+function Content(props: ContentProps) {
+  const { classes } = props;
+
+  return (
+    <Paper className={classes.paper}>
+      <AppBar className={classes.searchBar} position="static" color="default" elevation={0}>
+        <Toolbar>
+          <Grid container spacing={2} alignItems="center">
+            <Grid item>
+              <SearchIcon className={classes.block} color="inherit" />
+            </Grid>
+            <Grid item xs>
+              <TextField
+                fullWidth
+                placeholder="Search by email address, phone number, or user UID"
+                InputProps={{
+                  disableUnderline: true,
+                  className: classes.searchInput,
+                }}
+              />
+            </Grid>
+            <Grid item>
+              <Button variant="contained" color="primary" className={classes.addUser}>
+                Add user
+              </Button>
+              <Tooltip title="Reload">
+                <IconButton>
+                  <RefreshIcon className={classes.block} color="inherit" />
+                </IconButton>
+              </Tooltip>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.contentWrapper}>
+        <Typography color="textSecondary" >
+        
+
+
+    <div>
+     
+      
+     <Switch>
+       <Route path={ RoutePaths.DASHBOARD } component={ DashboardContainer } />
+       <Route path={ RoutePaths.USER } component={ UserContainer } />
+       <Route path={ RoutePaths.VENUE } component={ VenueContainer } />
+       <Route path={ RoutePaths.RESERVATION } component={ ReservationContainer } />
+       <Redirect exact from={ RoutePaths.MAIN }  to={ RoutePaths.DASHBOARD } />
+     </Switch>
+   </div> 
+
+
+
+        </Typography>
+      </div>
+    </Paper>
+  );
+}
+
+export default withStyles(styles)(Content);
