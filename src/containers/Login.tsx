4970ad1b826e000117f2a00957615f49b39a4075
mlph-pyap <mlph-pyap@mlph.168.1.13>
2020-07-28 11:03:22 +0800

    Update for page redirect

--- src/containers/Login.tsx
+++ src/containers/Login.tsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import { Button, FormControl, Form } from "react-bootstrap";
+import { useHistory } from 'react-router-dom'
 
 import {FieldValidation}   from "components/validation";
-
+import RoutePaths from 'constants/RoutePaths'
 import "./Login.css";
 
+
 export default function Login () {
+  const history = useHistory();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -33,9 +37,30 @@ export default function Login () {
     
 
   function handleSubmit( event:React.FormEvent<HTMLFormElement> ) {
-    event.currentTarget.action = "eo/";
-    event.currentTarget.onSubmit();
-    event.preventDefault();
+
+          fetch("http://localhost:8080/login",{
+            body:JSON.stringify({
+              "email":email,
+              "password":password
+            }),
+            method: 'POST',
+            mode: "cors", // Type of mode of the request
+          cache: "no-cache", // options like default, no-cache, reload, force-cache
+        //  credentials: "same-origin"
+          })
+          .then(res => {
+            return res.json();
+          })
+          .then((result) => {
+            const data = JSON.parse(result);
+            if(data.statuscode ==1){
+              history.replace(RoutePaths.MAIN);
+            }else{
+              alert(data.statusmessage);
+            }
+            
+          });
+          event.preventDefault();
   }
 
   return (
